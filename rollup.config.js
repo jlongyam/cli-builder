b643a9675cad94383ccc1a2ec88963b776bc8102
jlongyam <jhonliongbox@gmail.com>
2025-07-25 21:30:11 +0700

    Add README

--- rollup.config.js
+++ rollup.config.js
@@ -3,45 +3,16 @@ import commonjs from '@rollup/plugin-commonjs';
 import pkg from './package.json' with { type: 'json' };
 
 export default [
-	// browser-friendly UMD build
-	// {
-	// 	input: 'src/main.js',
-	// 	output: {
-	// 		name: 'howLongUntilLunch',
-	// 		file: pkg.browser,
-	// 		format: 'umd'
-	// 	},
-	// 	plugins: [
-	// 		resolve(), // so Rollup can find `ms`
-	// 		commonjs() // so Rollup can convert `ms` to an ES module
-	// 	]
-	// },
-
-	// CommonJS (for Node) and ES module (for bundlers) build.
-	// (We could have three entries in the configuration array
-	// instead of two, but it's quicker to generate multiple
-	// builds from a single configuration where possible, using
-	// an array for the `output` option, where we can specify
-	// `file` and `format` for each target)
-	// {
-	// 	input: 'src/main.js',
-	// 	external: ['ms'],
-	// 	output: [
-	// 		{ file: pkg.main, format: 'cjs' },
-	// 		{ file: pkg.module, format: 'es' }
-	// 	]
-	// }
 	{
 		input: './src/args.js',
 		output: {
 			file: pkg.main,
 			format: 'es',
-			// sourcemap: !isProduction,
 			inlineDynamicImports: true
 		},
 		plugins: [
-			resolve(), // so Rollup can find `ms`
-			commonjs() // so Rollup can convert `ms` to an ES module
+			resolve(),
+			commonjs()
 		]
 	}
 ];
